480acb7b4c9cc74ce1852bafcf66f860e73d6871
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:44:00 -0600

    Rewriting as Laminas Project package

--- test/TestAsset/SharedMockAggregate.php
+++ test/TestAsset/SharedMockAggregate.php
@@ -1,19 +1,18 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-eventmanager for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-eventmanager/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-eventmanager/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\EventManager\TestAsset;
+namespace LaminasTest\EventManager\TestAsset;
 
-use Zend\EventManager\SharedEventManagerInterface;
-use Zend\EventManager\SharedListenerAggregateInterface;
+use Laminas\EventManager\SharedEventManagerInterface;
+use Laminas\EventManager\SharedListenerAggregateInterface;
 
 /**
- * @group      Zend_EventManager
+ * @group      Laminas_EventManager
  */
 class SharedMockAggregate implements SharedListenerAggregateInterface
 {
