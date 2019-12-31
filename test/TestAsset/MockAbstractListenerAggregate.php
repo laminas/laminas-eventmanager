8899d33e7adfc48de140bb6b9810368fa2d8bef4
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:43:52 -0600

    Rewriting as Laminas Project package

--- test/TestAsset/MockAbstractListenerAggregate.php
+++ test/TestAsset/MockAbstractListenerAggregate.php
@@ -1,19 +1,18 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-eventmanager for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-eventmanager/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-eventmanager/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\EventManager\TestAsset;
+namespace LaminasTest\EventManager\TestAsset;
 
-use Zend\EventManager\AbstractListenerAggregate;
-use Zend\EventManager\EventManagerInterface;
+use Laminas\EventManager\AbstractListenerAggregate;
+use Laminas\EventManager\EventManagerInterface;
 
 /**
- * @group      Zend_EventManager
+ * @group      Laminas_EventManager
  */
 class MockAbstractListenerAggregate extends AbstractListenerAggregate
 {
