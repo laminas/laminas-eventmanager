265bec72f3307be6f77bf3f73c293fea80506f50
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:43:39 -0600

    Rewriting as Laminas Project package

--- src/EventInterface.php
+++ src/EventInterface.php
@@ -1,22 +1,20 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_EventManager
+ * @see       https://github.com/laminas/laminas-eventmanager for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-eventmanager/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-eventmanager/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\EventManager;
+namespace Laminas\EventManager;
 
 use ArrayAccess;
 
 /**
  * Representation of an event
  *
- * @category   Zend
- * @package    Zend_EventManager
+ * @category   Laminas
+ * @package    Laminas_EventManager
  */
 interface EventInterface
 {
