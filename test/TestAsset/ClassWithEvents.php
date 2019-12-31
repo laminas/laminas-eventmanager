f0a89984a557939a039775bf6fe1851edb80ac5a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:43:53 -0600

    Rewriting as Laminas Project package

--- test/TestAsset/ClassWithEvents.php
+++ test/TestAsset/ClassWithEvents.php
@@ -1,23 +1,21 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_EventManager
+ * @see       https://github.com/laminas/laminas-eventmanager for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-eventmanager/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-eventmanager/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\EventManager\TestAsset;
+namespace LaminasTest\EventManager\TestAsset;
 
-use Zend\EventManager\EventManagerInterface;
-use Zend\EventManager\EventManager;
+use Laminas\EventManager\EventManager;
+use Laminas\EventManager\EventManagerInterface;
 
 /**
- * @category   Zend
- * @package    Zend_EventManager
+ * @category   Laminas
+ * @package    Laminas_EventManager
  * @subpackage UnitTests
- * @group      Zend_EventManager
+ * @group      Laminas_EventManager
  */
 class ClassWithEvents
 {
