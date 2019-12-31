86516e350610ec04508d85e201e461fd9ccf0bc2
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:43:56 -0600

    Rewriting as Laminas Project package

--- test/FilterChainTest.php
+++ test/FilterChainTest.php
@@ -1,19 +1,18 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-eventmanager for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-eventmanager/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-eventmanager/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Stdlib;
+namespace LaminasTest\Stdlib;
 
-use Zend\EventManager\FilterChain;
-use Zend\Stdlib\CallbackHandler;
+use Laminas\EventManager\FilterChain;
+use Laminas\Stdlib\CallbackHandler;
 
 /**
- * @group      Zend_Stdlib
+ * @group      Laminas_Stdlib
  */
 class FilterChainTest extends \PHPUnit_Framework_TestCase
 {
@@ -137,6 +136,6 @@ class FilterChainTest extends \PHPUnit_Framework_TestCase
 
     public function filterReceivalCallback($context, array $params, $chain)
     {
-        $this->assertInstanceOf('Zend\EventManager\Filter\FilterIterator', $chain);
+        $this->assertInstanceOf('Laminas\EventManager\Filter\FilterIterator', $chain);
     }
 }
