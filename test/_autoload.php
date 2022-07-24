36c45fde3e69306de5e7dc9660fddcb9bd7eb02e
George Steel <george@net-glue.co.uk>
2022-07-24 09:30:39 +0100

    Initialise psalm, update dotfiles, remove useless PHPUnit bootstrap and outdated config
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/_autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use PHPUnit\Framework\ExpectationFailedException;
-
-if (class_exists('PHPUnit_Framework_ExpectationFailedException')) {
-    class_alias('PHPUnit_Framework_ExpectationFailedException', ExpectationFailedException::class);
-}
